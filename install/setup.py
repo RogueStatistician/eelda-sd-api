d528e629043bc32969e4959e882f88eae1ab9af2
RogueStatistician <simone.tosato95@gmail.com>
2021-07-14 18:04:14 +0200

    installer for showdown changes

---
+++ install/setup.py
@@ -0,0 +1,36 @@
+import os
+import sys, getopt
+import shutil
+
+def main(argv):
+	usage = 'setup.py -p <path-to-pokemon-showdown>'
+	basedir = os.path.dirname(__file__)
+	showdown_root = ''
+	try:
+		opts,args = getopt.getopt(argv,"hp:",["path="])
+	except getopt.GetoptError:
+		print(usage)
+		sys.exit(2)
+	for opt,arg in opts:
+		if opt == '-h':
+			print(usage)
+			sys.exit()
+		elif opt in ('-p','--path'):
+			showdown_root = arg
+		else:
+			print('required argument -i missing')
+			print(usage)
+	installer_files = []
+	for path,subdirs,files in os.walk(os.path.join(basedir,'..','pokemon-showdown')):
+		for name in files:
+			installer_files.append(os.path.join(path,name))
+	for file in installer_files:
+		path_to_file = file.split('../pokemon-showdown/')[1]
+		print(path_to_file)
+		os.rename(os.path.join(showdown_root,path_to_file),os.path.join(showdown_root,path_to_file)+'.old')
+		shutil.copy(file,os.path.join(showdown_root,path_to_file))
+
+if __name__ == "__main__":
+	main(sys.argv[1:])
+
+
