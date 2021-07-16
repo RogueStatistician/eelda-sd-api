c75cfbe3995492f694befaa451a2d77701157762
RogueStatistician <simone.tosato95@gmail.com>
2021-07-16 13:01:22 +0200

    weird error

--- install/setup.py
+++ install/setup.py
@@ -1,6 +1,7 @@
 import os
 import sys, getopt
 import shutil
+import sqlite3
 
 def main(argv):
 	usage = 'setup.py -p <path-to-pokemon-showdown>'
@@ -25,11 +26,18 @@ def main(argv):
 	for path,subdirs,files in os.walk(os.path.join(basedir,'..','pokemon-showdown')):
 		for name in files:
 			installer_files.append(os.path.join(path,name))
+
+	print('+--------------------------------------------+')
+	print('|   Eelda\'s League Showdown Customization   |')
+ 	print('+--------------------------------------------+')
+	print('Copying custom files to pokemon-showdown')
 	for file in installer_files:
 		path_to_file = file.split('../pokemon-showdown/')[1]
 		print(path_to_file)
 		os.rename(os.path.join(showdown_root,path_to_file),os.path.join(showdown_root,path_to_file)+'.old')
 		shutil.copy(file,os.path.join(showdown_root,path_to_file))
+	print('Database creation')
+	db = sqlite3.connect(os.path.join(basedir,'..','db','showdown.db'))
 
 if __name__ == "__main__":
 	main(sys.argv[1:])
