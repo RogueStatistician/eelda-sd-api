dc768f2b51f93b5c267886e289420bb1558131ff
RogueStatistician <simone.tosato95@gmail.com>
2021-07-19 14:04:56 +0200

    buildin db

--- install/setup.py
+++ install/setup.py
@@ -7,7 +7,33 @@ from collections import OrderedDict
 
 def build_db(showdown_root,basedir):
 	abilities = 'CREATE TABLE IF NOT EXISTS abilities (ID INT PRIMARY KEY, ability TEXT NOT NULL)'
-	
+	db = sqlite3.connect(os.path.join(basedir,'..','db','showdown.db'))
+	dex = open(os.path.join(showdown_root,'data','pokedex.ts'),'r')
+	dex = '{\n'+''.join(dex.readlines()[1:-1])+'}'
+	dex_db = hjson.loads(dex)
+	cursor = db.cursor()
+	keys = set()
+	for key in dex_db:
+		keys.update(list(dex_db[key].keys()))
+	print(key)
+	# create_query = "create table if not exists pokedex ({0})".format(" text,".join(keys))
+	# cursor.execute(create_query)
+	# columns = ', '.join(keys)
+	# placeholders = ':'+', :'.join(keys)
+	# query = 'INSERT INTO pokedex (%s) VALUES (%s)' % (columns, placeholders)
+	# for key in dex_db:
+	# 	tmp = dict.fromkeys(keys)
+	# 	tmp.update(dex_db[key])
+	# 	print(key)
+	# 	for key in tmp:
+	# 		if isinstance(tmp[key],OrderedDict):
+	# 			tmp[key]=str(dict(tmp[key]))
+	# 		if isinstance(tmp[key],list):
+	# 			tmp[key]=str(tmp[key])
+	# 		#print(key+' '+str(type(tmp[key])))
+	# 	cursor.execute(query, tmp)
+	# db.commit()
+		
 
 def main(argv):
 	usage = 'setup.py -p <path-to-pokemon-showdown>'
@@ -43,31 +69,7 @@ def main(argv):
 		os.rename(os.path.join(showdown_root,path_to_file),os.path.join(showdown_root,path_to_file)+'.old')
 		shutil.copy(file,os.path.join(showdown_root,path_to_file))
 	print('\n### Database creation ###\n')
-	db = sqlite3.connect(os.path.join(basedir,'..','db','showdown.db'))
-	dex = open(os.path.join(showdown_root,'data','pokedex.ts'),'r')
-	dex = '{\n'+''.join(dex.readlines()[1:-1])+'}'
-	dex_db = hjson.loads(dex)
-	cursor = db.cursor()
-	keys = set()
-	for key in dex_db:
-		keys.update(list(dex_db[key].keys()))
-	create_query = "create table if not exists pokedex ({0})".format(" text,".join(keys))
-	cursor.execute(create_query)
-	columns = ', '.join(keys)
-	placeholders = ':'+', :'.join(keys)
-	query = 'INSERT INTO pokedex (%s) VALUES (%s)' % (columns, placeholders)
-	for key in dex_db:
-		tmp = dict.fromkeys(keys)
-		tmp.update(dex_db[key])
-		print(key)
-		for key in tmp:
-			if isinstance(tmp[key],OrderedDict):
-				tmp[key]=str(dict(tmp[key]))
-			if isinstance(tmp[key],list):
-				tmp[key]=str(tmp[key])
-			#print(key+' '+str(type(tmp[key])))
-		cursor.execute(query, tmp)
-	db.commit()
+	build_db(showdown_root,basedir)
 
 
 
